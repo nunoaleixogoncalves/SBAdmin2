5129a4e4c3477be4e278de9abf84b186ec28688d
Nuno Gonçalves <nuno.goncalves@airc.pt>
2020-02-06 15:53:27 +0000

    wip

--- template/src/app/app-routing.module.ts
+++ template/src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { BaseComponent } from './base/base.component';
 
 
 const routes: Routes = [
-  {path : '', component : LoginComponent},
-  {path : 'login', component : LoginComponent},
-  {path: '', component: BaseComponent,
+  {
+    path: '', component: BaseComponent,
     children: [
-      {path : 'blank', component : BlankComponent}
+      { path: '', component: BlankComponent },
+      { path: 'blank', component: BlankComponent }
     ]
-  }
+  },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- template/src/app/sidebar/sidebar.component.ts
+++ template/src/app/sidebar/sidebar.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from './../../environments/environment';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent implements OnInit {
-  hidden: boolean;
+export class SidebarComponent {
   constructor() {
-    this.hidden = environment.hidden;
   }
 
-  ngOnInit() {
+  sidebarToggle() {
+    const classBody = document.getElementById('page-top').className;
+    if (classBody === 'sidebar-toggled') {
+      document.getElementById('page-top').className = '';
+      document.getElementById('accordionSidebar').className = 'navbar-nav bg-gradient-primary sidebar sidebar-dark accordion';
+    } else {
+      document.getElementById('page-top').className = 'sidebar-toggled';
+      document.getElementById('accordionSidebar').className = 'navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled';
+    }
   }
-
 }
